572b1d0a427ea022a37010c19e582b4679458faa
Nadun De Silva <nadunrds@gmail.com>
2022-07-09 23:04:13 +0530

    Add webhook for setting defaults

---
+++ api/v1alpha1/microservice_webhook.go
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2022, Nadun De Silva. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/pointer"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+)
+
+// log is for logging in this package.
+var microservicelog = logf.Log.WithName("microservice-resource")
+
+func (r *Microservice) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
+
+// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+
+//+kubebuilder:webhook:path=/mutate-mesh-manager-nadundesilva-github-io-v1alpha1-microservice,mutating=true,failurePolicy=fail,sideEffects=None,groups=mesh-manager.nadundesilva.github.io,resources=microservices,verbs=create;update,versions=v1alpha1,name=mmicroservice.kb.io,admissionReviewVersions=v1
+
+var _ webhook.Defaulter = &Microservice{}
+
+// Default implements webhook.Defaulter so a webhook will be registered for the type
+func (r *Microservice) Default() {
+	microservicelog.Info("default", "name", r.Name)
+
+	if r.Spec.Replicas == nil {
+		r.Spec.Replicas = pointer.Int32(1)
+	}
+	for _, dependency := range r.Spec.Dependencies {
+		if dependency.Namespace == "" {
+			dependency.Namespace = r.GetNamespace()
+		}
+	}
+}
+
+// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
+//+kubebuilder:webhook:path=/validate-mesh-manager-nadundesilva-github-io-v1alpha1-microservice,mutating=false,failurePolicy=fail,sideEffects=None,groups=mesh-manager.nadundesilva.github.io,resources=microservices,verbs=create;update,versions=v1alpha1,name=vmicroservice.kb.io,admissionReviewVersions=v1
+
+var _ webhook.Validator = &Microservice{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *Microservice) ValidateCreate() error {
+	microservicelog.Info("validate create", "name", r.Name)
+	return r.validate()
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *Microservice) ValidateUpdate(old runtime.Object) error {
+	microservicelog.Info("validate update", "name", r.Name)
+	return r.validate()
+}
+
+func (r *Microservice) validate() error {
+	return nil
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *Microservice) ValidateDelete() error {
+	microservicelog.Info("validate delete", "name", r.Name)
+	return nil
+}
--- main.go
+++ main.go
@@ -86,6 +86,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Microservice")
 		os.Exit(1)
 	}
+	if err = (&meshmanagerv1alpha1.Microservice{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Microservice")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
